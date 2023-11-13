162fd2d78723db39d3f31e015121f816aef76628
Karolina Tofil <karolinaatofil@gmail.com>
2023-11-14 00:39:44 +0100

    velocity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -21,28 +23,24 @@ public class PlayerController : MonoBehaviour
     {   //Debug.Log(Time.deltaTime);
         //transform.Translate(xMoveSpeed * Time.deltaTime, yMoveSpeed * Time.deltaTime, zMoveSpeed * Time.deltaTime);
 
-
         Movement();
     }
 
     private void Movement()
     {
         Vector3 newPosition = new Vector3();
-
+        
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             newPosition = new Vector3(0f, 0f, zMoveSpeed);
-            //transform.Translate(0f, 0f, zMoveSpeed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             newPosition = new Vector3(0f, 0f, -zMoveSpeed);
-            //transform.Translate(0f, 0f, -zMoveSpeed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             newPosition = new Vector3(xMoveSpeed, 0f, 0f);
-            //transform.Translate(xMoveSpeed * Time.deltaTime, 0f, 0f);
         }
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
@@ -50,7 +48,10 @@ public class PlayerController : MonoBehaviour
             //transform.Translate(-xMoveSpeed * Time.deltaTime, 0f, 0f);
         }
 
-        transform.position += newPosition * Time.deltaTime;
+        GetComponent<Rigidbody>().velocity = newPosition;
+
+
+        //transform.position += newPosition * Time.deltaTime;
     }
 
 }
